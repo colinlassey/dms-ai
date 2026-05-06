bc28f172791d8c4a2ec6f5d1615bee9c0c10753f
Colin Lassey <colinlassey@Colins-Mac-mini-4.local>
2026-05-06 16:26:11 -0400

    new test lol

--- camera.py
+++ camera.py
@@ -2,6 +2,12 @@ from picamera2 import Picamera2
 import time
 
 cam = Picamera2()
+cam.configure(cam.create_preview_configuration())
 cam.start()
 
-print(cam.camera_config)
+print("camera initialized")
+
+cam.capture_file("test.jpg")
+print("image captured successfully")
+
+cam.stop()
